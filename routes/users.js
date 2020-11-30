4caadae0dbedf040e10d174adeac255a2ab942a5
mono404 <kwh6543@hanmail.net>
2020-11-30 16:30:30 +0900

    modify users.js

--- routes/users.js
+++ routes/users.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render("user/my_info.html");
+  res.render("users/my_info.html");
 });
 
 router.get('/login', function(req, res, next) {
-  res.render("user/login.html");
+  res.render("users/login.html");
 });
 
 router.get('/register', function(req, res, next) {
-  res.render("user/register.html");
+  res.render("users/register.html");
 });
 
 router.get('/find_id', function(req, res, next) {
-  res.render("user/find_id.html");
+  res.render("users/find_id.html");
 });
 
 router.get('/find_passwd', function(req, res, next) {
-  res.render("user/find_passwd.html");
+  res.render("users/find_passwd.html");
 });
 
 router.get('/my_info', function(req, res, next) {
-  res.render("user/my_info.html");
+  res.render("users/my_info.html");
 });
 
 module.exports = router;
