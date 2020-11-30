b9631215bae0f5a6fcbfd09f2317f1f121628bfa
mono404 <kwh6543@hanmail.net>
2020-11-30 11:20:08 +0900

    add routers

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('main.html', { title: 'Express' });
 });
 
-router.get('/index', function(req, res, next) {
-  res.render('index.html', { title: 'mono' });
-});
-
-router.get('/test', function(req, res, next) {
-  res.render('test.html', { title: req.query.id });
-});
-
-// router.get('/item_list', function(req, res, next) {
-//   res.render('item_list.html', { title: 'Express' });
-// });
-
 module.exports = router;
