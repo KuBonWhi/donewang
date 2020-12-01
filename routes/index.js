2a7353cadf5136119c28cf230a75ab76a3be6c15
mono404 <kwh6543@hanmail.net>
2020-12-01 20:37:13 +0900

    commit

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('main.html', { title: 'Express' });
 });
 
-router.get('/test', function(req, res, next) {
-  res.render('test.html', { title: req.query.id });
-});
-
-
-
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -7,24 +7,57 @@ router.get('/', function(req, res, next) {
   res.render("user/my_info.html");
 });
 
-router.get('/login', function(req, res, next) {
-  res.render("user/login.html");
+router.post('/login',async (req,res,next)=>{
+  try{
+    let body = req.body;
+
+    let result = await model['member_info'].findOne({
+      where: {
+        id : body.id
+      }
+    });
+
+    // let dbPassword = result.dataValues.password;
+    // let inputPassword = body.password;
+    // let salt = result.dataValues.salt;
+    // let hashPassword = crypto.createHash("sha512").update(inputPassword + salt).digest("hex");
+
+
+    if(result.password == body.pw)
+    {
+      console.log("비밀번호 일치");
+      res.cookie("user", body.id , {
+        expires : new Date(Date.now() + 900000),
+        httpOnly : true
+      })
+      res.send("로그인 성공");
+    }
+    else
+    {
+      console.log("로그인 실패");
+      res.redirect("/");
+    }
+
+  }catch(err){
+    console.log(err);
+    next(err);
+  }
 });
 
 router.get('/register', function(req, res, next) {
-  res.render("user/register.html");
+  res.render("users/register.html");
 });
 
 router.get('/find_id', function(req, res, next) {
-  res.render("user/find_id.html");
+  res.render("users/find_id.html");
 });
 
 router.get('/find_passwd', function(req, res, next) {
-  res.render("user/find_passwd.html");
+  res.render("users/find_passwd.html");
 });
 
 router.get('/my_info', function(req, res, next) {
-  res.render("user/my_info.html");
+  res.render("users/my_info.html");
 });
 
 router.get('/findAllUsers', async(req, res, next) =>{
