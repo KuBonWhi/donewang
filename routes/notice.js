ba9fb845ecee0f5c97fd638a79edc44697f5c53b
wpffl <wpffl3333@naver.com>
2020-12-05 16:56:18 +0900

    link notice fold

--- routes/notice.js
+++ routes/notice.js
@@ -7,4 +7,16 @@ router.get('/notice', function(req, res, next) {
     res.render('notice/notice.html', { session: session });
 });
 
+router.get('/notice_detail', function(req, res, next) {
+    let session = req.session;
+
+    res.render('notice/notice_detail.html', { session: session });
+});
+
+router.get('/write_notice', function(req, res, next) {
+    let session = req.session;
+
+    res.render('notice/write_notice.html', { session: session });
+});
+
 module.exports = router;
