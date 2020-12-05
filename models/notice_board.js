878df2c0f5eecb02d242f6e512fd7d6dd0287574
dheenim <pdh1642@gmail.com>
2020-12-05 18:25:11 +0900

    new db needs, link items

---
+++ models/notice_board.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class notice_board extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  notice_board.init({
+    notice_id: {
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
+    title: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {
+    sequelize,
+    timestamps:false,
+    underscored:false,
+    createdAt:true,
+    charset:'utf8',
+    collate:'utf8_general_ci',
+    modelName: 'notice_board',
+  });
+  return notice_board;
+};
